f44e2470caadd24195f3904a3bde96c3a4544688
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-12-14 02:43:50 -0500

    Enhance loading state UI with a spinner for better user experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,11 @@ const App: React.FC = () => {
   }, [dispatch, token]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
   }
 
   return (
@@ -55,4 +59,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
